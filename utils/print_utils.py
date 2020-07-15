6833b261e5a30b722b8dd5af5715de85d77bece4
hezl1592 <hezl1592@163.com>
2020-07-15 15:18:42 +0800

    update

--- utils/print_utils.py
+++ utils/print_utils.py
@@ -5,13 +5,13 @@ __maintainer__ = "Sachin Mehta"
 import time
 
 text_colors = {
-               'logs': '\033[34m', # 033 is the escape code and 34 is the color code
-               'info': '\033[32m',
-               'warning': '\033[33m',
-               'error': '\033[31m',
-               'bold': '\033[1m',
-               'end_color': '\033[0m'
-               }
+    'logs': '\033[34m',  # 033 is the escape code and 34 is the color code
+    'info': '\033[32m',
+    'warning': '\033[33m',
+    'error': '\033[31m',
+    'bold': '\033[1m',
+    'end_color': '\033[0m'
+}
 
 
 def get_curr_time_stamp():
@@ -20,7 +20,7 @@ def get_curr_time_stamp():
 
 def print_error_message(message):
     time_stamp = get_curr_time_stamp()
-    error_str = text_colors['error'] + text_colors['bold'] + 'ERROR  ' + text_colors['end_color']
+    error_str = text_colors['error'] + text_colors['bold'] + ' ERROR ' + text_colors['end_color']
     print('{} - {} - {}'.format(time_stamp, error_str, message))
     print('{} - {} - {}'.format(time_stamp, error_str, 'Exiting!!!'))
     exit(-1)
@@ -28,7 +28,7 @@ def print_error_message(message):
 
 def print_log_message(message):
     time_stamp = get_curr_time_stamp()
-    log_str = text_colors['logs'] + text_colors['bold'] + 'LOGS   ' + text_colors['end_color']
+    log_str = text_colors['logs'] + text_colors['bold'] + ' LOGS  ' + text_colors['end_color']
     print('{} - {} - {}'.format(time_stamp, log_str, message))
 
 
@@ -40,8 +40,28 @@ def print_warning_message(message):
 
 def print_info_message(message):
     time_stamp = get_curr_time_stamp()
-    info_str = text_colors['info'] + text_colors['bold'] + 'INFO   ' + text_colors['end_color']
+    info_str = text_colors['info'] + text_colors['bold'] + ' INFO  ' + text_colors['end_color']
     print('{} - {} - {}'.format(time_stamp, info_str, message))
+
+
+def error_message(message):
+    error_str = text_colors['error'] + text_colors['bold'] + ' ERROR ' + text_colors['end_color']
+    return '- {} - {}'.format(error_str, message)
+
+
+def log_message(message):
+    log_str = text_colors['logs'] + text_colors['bold'] + ' LOGS  ' + text_colors['end_color']
+    return '- {} - {}'.format(log_str, message)
+
+
+def warning_message(message):
+    warn_str = text_colors['warning'] + text_colors['bold'] + 'WARNING' + text_colors['end_color']
+    return '- {} - {}'.format(warn_str, message)
+
+
+def info_message(message):
+    info_str = text_colors['info'] + text_colors['bold'] + ' INFO  ' + text_colors['end_color']
+    return '- {} - {}'.format(info_str, message)
 
 
 if __name__ == '__main__':
--- utils/train_utils.py
+++ utils/train_utils.py
@@ -2,3 +2,21 @@
 # Time    : 2020/7/15 13:32
 # Author  : zlich
 # Filename: train_utils.py
+import torch
+# from utils.print_utils import *
+from utils.log_utils import infoLogger
+
+def deviceSetting(logger):
+    num_gpus = torch.cuda.device_count()
+    device = 'cuda' if num_gpus > 0 else 'cpu'
+    if num_gpus >= 1:
+        logger.info("GPU found, device: {}, number: {}.".format(device, num_gpus))
+    else:
+        logger.warning("No GPU found, device: CPU.")
+        # print_warning_message()
+    return num_gpus, device
+
+
+if __name__ == '__main__':
+    logger = infoLogger(name="test")
+    deviceSetting(logger)
