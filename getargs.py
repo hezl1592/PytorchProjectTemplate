6efe4a3a16e0f6442dd38bf8a51c38bf41f51ecb
hezl1592 <hezl1592@163.com>
2020-07-30 17:29:26 +0800

    getargs

--- getargs.py
+++ getargs.py
@@ -5,12 +5,38 @@
 import argparse
 import sys
 from utils.train_utils import readYAML
+from easydict import EasyDict
+import os
+import yaml
+from collections import OrderedDict
+
+
+def loadConfig(cfgPath):
+    '''
+    :param cfgPath: str, path(better absolute path)
+    :return: modelConfig: dict, config
+    # Loader=yaml.FullLoader
+    '''
+    with open(cfgPath) as f:
+        modelConfig = yaml.load(f, Loader=yaml.FullLoader)
+    # print(modelConfig)
+    return argparse.Namespace(**modelConfig)
+
+
+def cfgInfo(Cfg: argparse.Namespace):
+    outstr = "Config:\n"
+    outstr += '--' * 30 + '\n'
+    for key, value in Cfg.__dict__.items():
+        outstr += "{:10s}: {}\n".format(key, value)
+    return outstr + '--' * 30
 
 
 def getArgs(argv):
     sys.argv = argv
     # print(sys.argv)
     parser = argparse.ArgumentParser()
+    parser.add_argument("--config", default="config/bdd100k_deeplabv3p_mobilenetv2_apex.yaml",
+                        type=str, help="model config path")
 
     # main
     parser.add_argument("--model", default="deeplabv3+", type=str, help="model name")
@@ -27,6 +53,47 @@ def getArgs(argv):
     parser.add_argument('--freeze_bn', default=False, action='store_true', help='Freeze BN params or not')
 
     args = parser.parse_args()
-    # for name, value in vars(args).items():
-    #     print(name, value)
+    yamlConfig = loadConfig(args.config)
+    yamlConfig.__dict__.update(args.__dict__)
+
+    return yamlConfig
+
+
+def getArgs_(argv, configPath=None):
+    # 该函数仅通过
+    sys.argv = argv
+    if len(argv) == 1:
+        if not configPath:
+            assert False, "please check, no config file!"
+        else:
+            pass
+    else:
+        if not configPath:
+            if 'config' not in argv[1]:
+                assert False, "please check, no config file!"
+            configPath = argv[1].split('=')[-1]
+            sys.argv.pop(1)
+        else:
+            if 'config' not in argv[1]:
+                pass
+            else:
+                configPath = argv[1].split('=')[-1]
+                sys.argv.pop(1)
+
+    yamlConfig = loadConfig(configPath)
+
+    parser = argparse.ArgumentParser()
+    for name, value in vars(yamlConfig).items():
+        parser.add_argument("--{}".format(name), default=value,
+                            type=type(value), help="{}".format(name))
+
+    args = parser.parse_args()
+    if isinstance(args.size, str):
+        args.size = eval(args.size)
+
     return args
+
+
+if __name__ == '__main__':
+    a = getArgs(sys.argv)
+    printInfo(a)
